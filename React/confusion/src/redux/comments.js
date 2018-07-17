b5a6f1d865e0babb201987ddcf78fdda953acf00
Nirav Trivedi <niravtrivedi2112@outlook.com>
2018-07-17 13:05:14 +0530

    combining reducers

---
+++ React/confusion/src/redux/comments.js
@@ -0,0 +1,9 @@
+import { COMMENTS } from '../shared/comments'
+
+
+export const Comments = (state = COMMENTS ,actions) => {
+    switch(actions.type ) {
+        default : 
+            return state;
+    }
+}
---
+++ React/confusion/src/redux/dishes.js
@@ -0,0 +1,10 @@
+import { DISHES } from '../shared/dishes'
+// import { actions } from '../../node_modules/react-redux-form';
+
+
+export const Dishes = (state = DISHES ,actions) => {
+    switch(actions.type ) {
+        default : 
+            return state;
+    }
+}
