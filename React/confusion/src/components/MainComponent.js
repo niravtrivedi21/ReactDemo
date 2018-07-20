e6efa15545917971a151e44d4f25bdf3be48f35e
Nirav Trivedi <niravtrivedi2112@gmail.com>
2018-07-20 22:10:42 +0530

    redux thunk

--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Footer from './FooterComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import DishDetail from './DishdetailComponent';
 import { connect } from 'react-redux';
-import { addComment } from '../redux/ActionCreators';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 
 const mapStateTOProps = state => {
 
@@ -23,23 +23,30 @@ const mapStateTOProps = state => {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    addComment:(dishId,rating,author,comment) => dispatch(addComment(dishId,rating,author,comment))
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+    fetchDishes: () => { dispatch(fetchDishes()) }
 });
 
 class Main extends Component {
 
-    // constructor(props) {
-    //     super(props);
+    constructor(props) {
+        super(props);
 
 
-    // }
+    }
+
+    componentDidMount() {
+        this.props.fetchDishes();
+    }
 
 
     render() {
 
         const HomePage = () => {
             return (
-                <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+                <Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                    dishesLoading={this.props.dishes.isLoading}
+                    dishesErrMess={this.props.dishes.errMess}
                     promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
                     leader={this.props.leaders.filter((leader) => leader.featured)[0]}
                 />
@@ -48,9 +55,11 @@ class Main extends Component {
 
         const DishWithId = ({ match }) => {
             return (
-                <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    isLoading={this.props.dishes.isLoading}
+                    errMess={this.props.dishes.errMess}
                     comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
-                    addComment = {this.props.addComment}
+                    addComment={this.props.addComment}
                 />
             );
         }
@@ -72,4 +81,4 @@ class Main extends Component {
     }
 }
 
-export default withRouter(connect(mapStateTOProps,mapDispatchToProps)(Main));
+export default withRouter(connect(mapStateTOProps, mapDispatchToProps)(Main));
--- React/confusion/src/redux/dishes.js
+++ React/confusion/src/redux/dishes.js
@@ -1,10 +1,24 @@
-import { DISHES } from '../shared/dishes'
+/// import { DISHES } from '../shared/dishes'
 // import { actions } from '../../node_modules/react-redux-form';
+import * as ActionTypes from './ActionTypes';
 
 
-export const Dishes = (state = DISHES ,actions) => {
-    switch(actions.type ) {
-        default : 
+export const Dishes = (state = {
+    isLoading: true,
+    errMess: null,
+    dishes: []
+}, actions) => {
+    switch (actions.type) {
+        case ActionTypes.ADD_DISHES:
+            return { ...state, isLoading: false, errMess: null, dishes: actions.payload };
+
+        case ActionTypes.DISHES_FAILED:
+            return { ...state, isLoading: false, errMess: actions.payload, dishes: [] };
+
+        case ActionTypes.DISHES_LOADING:
+            return { ...state, isLoading: true, errMess: null, dishes: [] };
+
+        default:
             return state;
     }
 }
