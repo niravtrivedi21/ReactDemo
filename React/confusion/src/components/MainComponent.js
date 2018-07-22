89c4d257750077996db11619a33fb799e84ed4c4
Nirav Trivedi <niravtrivedi2112@gmail.com>
2018-07-22 23:20:54 +0530

    react animation

--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -10,7 +10,8 @@ import DishDetail from './DishdetailComponent';
 import { connect } from 'react-redux';
 import { fetchDishes, fetchComments , fetchPromos, postComment } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
-
+import { TransitionGroup , CSSTransition  } from 'react-transition-group';
+ 
 const mapStateTOProps = state => {
 
     return {
@@ -78,6 +79,8 @@ class Main extends Component {
         return (
             <div >
                 <Header />
+                <TransitionGroup>
+                    <CSSTransition key = {this.props.location.key} classNames="page" timeout={300}>
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
@@ -86,6 +89,8 @@ class Main extends Component {
                     <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
                     <Redirect to="/home" />
                 </Switch>
+                </CSSTransition>
+                </TransitionGroup>
                 <Footer />
             </div>
         );
