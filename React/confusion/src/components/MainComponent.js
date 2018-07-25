b494f14207f36b0ad9a0642944f8c9a7d59b5dd8
Nirav Trivedi <niravtrivedi2112@gmail.com>
2018-07-25 20:51:06 +0530

    project

--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -38,11 +38,11 @@ const mapDispatchToProps = (dispatch) => ({
 
 class Main extends Component {
 
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
 
 
-    }
+    // }
 
     componentDidMount() {
         this.props.fetchDishes();
